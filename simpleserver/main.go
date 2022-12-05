a48d5eb83124218b4ea6ae108be91772d1972ff5
Michele Mendel <michelemendel@gmail.com>
2022-12-05 20:45:51 +0100

    misc

--- simpleserver/main.go
+++ simpleserver/main.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+// func main() {
+// 	http.HandleFunc("/", rootHandler)
+// 	log.Fatal(http.ListenAndServe(":8888", nil))
+// }
+
+// func rootHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi %s", "back")
+// }
+
+func setupRoutes(app *fiber.App) {
+	// Define and map a route
+	app.Get("/", homePage)
+}
+
+func homePage(ctx *fiber.Ctx) error {
+	// Create response
+	msg := "Welcome to Web Server Using Fiber"
+	// Send
+	return ctx.SendString(msg)
+}
+
 func main() {
-	http.HandleFunc("/", rootHandler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
-}
+	// Create new fiber instance
+	app := fiber.New()
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi %s", "back")
-}
+	// Setup routes
+	setupRoutes(app)
 
-// Hello
-// bye
-// all bye of me
+	// Start fiber app
+	fmt.Println("Server is running on port :3000")
+	app.Listen(":3000")
+}
